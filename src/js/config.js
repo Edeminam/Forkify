c7fc8b8ba23edeaa103bdcef2465db0d3a713834
edeminam <elebrendan@gmail.com>
2025-02-12 02:30:21 +0100

    Initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fa5855ee-7121-4f5a-8165-8567fffc4be7';
+export const MODAL_CLOSE_SEC = 2.5;
